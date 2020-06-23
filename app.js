fd5073fc7968d8029f47c8c1563378ae10877c5f
Devjit Bedi <devjitbedi@Devjits-MBP.lan1>
2020-06-22 17:37:22 -0700

    1.16

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 
+$( document ).ready(function() {
+
+
+  setTimeout(function() { 
+
+    $(".loading-container p").css("opacity", "1");
+
+
+  }, 3000);
+
+  setTimeout(function() { 
+
+    $(".loading-container p").css("opacity", "0");
+
+
+  }, 5000);
+
+});
+
+
+
 $(window).on("load", function () {
 
 
